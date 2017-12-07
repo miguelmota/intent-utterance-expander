3d799f68a42ef1fef790f39b12c19b15e3dcbc64
Steve Androulakis <steve.androulakis@gmail.com>
2017-12-07 15:33:28 +1100

    Fix for issues/1
    Code by @zivbrachia
    
    https://github.com/miguelmota/intent-utterance-expander/issues/1#issuecomment-345520614

--- intent-utterance-expander.js
+++ intent-utterance-expander.js
@@ -63,12 +63,22 @@
                 phrases.push(copy);
 
                 // remove word, ie. (|foo)
-                if (words.length === 1) {
-                  copy = parts.slice(0);
-                  copy.splice(i, 1);
-                  phrases.push(copy);
-                }
               }
+            }
+
+            if (part.startsWith("(|")) {
+                          copy = parts.slice(0);
+                          copy.splice(i, 1);
+                          phrases.push(copy);
+            } else if (part.endsWith("|)")) {
+                          copy = parts.slice(0);
+                          copy.splice(i, parts.length);
+                          phrases.push(copy);
+            } else if (part.includes("||")) {
+                          copy = parts.slice(0);
+                          let a = copy.slice(0, i);
+                          let b = copy.slice(i + 1, parts.length);
+                          phrases.push(a.concat(b));
             }
 
             break;
